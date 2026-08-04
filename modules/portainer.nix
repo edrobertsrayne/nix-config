c1d1b1d80742f7856637709099cb1ef643c20b6b
Ed Roberts Rayne <ed.rayne@gmail.com>
2026-08-04 17:37:15 +0000

    fix(portainer): update healthcheck endpoint

--- modules/portainer.nix
+++ modules/portainer.nix
@@ -10,7 +10,7 @@ in {
         group = "Infrastructure";
         description = "Container manager";
         icon = "portainer.png";
-        probePath = "/api/status";
+        probePath = "/api/system/status";
         extraConfig = ''
           proxy_set_header X-Forwarded-Port $server_port;
           proxy_buffering off;
