f9b56f55d40745ebc8765a0c07860296c7f162e1
Ed Roberts Rayne <96839304+edrobertsrayne@users.noreply.github.com>
2026-08-16 13:49:18 +0100

    Clean up comments in bridge.nix
    Removed comments regarding network configuration changes.

--- modules/hosts/thor/bridge.nix
+++ modules/hosts/thor/bridge.nix
@@ -6,9 +6,6 @@
       networkmanager.enable = lib.mkForce false;
       useDHCP = false;
       bridges = {
-        # vm-mimir is microvm.nix's tap interface for the mimir guest
-        # (modules/hosts/mimir/mimir.nix). It puts mimir directly on the LAN,
-        # the same as the physical NICs.
         "br0" = {
           interfaces = ["enp2s0" "enp3s0" "enp4s0" "enp5s0" "vm-mimir"];
         };
@@ -25,9 +22,5 @@
       };
       nameservers = ["194.242.2.2" "1.1.1.1" "8.8.8.8"];
     };
-
-    # thor no longer has an exit-node table to bypass. #203 moved Mullvad's exit
-    # node to mimir, so the 40-br0 routingPolicyRules bypass rule that this file
-    # used to carry is also gone.
   };
 }
